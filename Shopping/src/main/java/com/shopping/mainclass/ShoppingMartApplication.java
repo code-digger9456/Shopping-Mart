d9193d92898acafb0742089dd439115f22ee4a70
shiva <shiva@HP>
2024-02-20 18:51:49 +0530

    project push

---
+++ Shopping/src/main/java/com/shopping/mainclass/ShoppingMartApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.mainclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingMartApplication.class, args);
+	}
+
+}
